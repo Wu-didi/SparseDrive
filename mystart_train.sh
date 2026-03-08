b0d6dd712f6f7ed07223ac465557aabb2d7e707a
wudi <164662525@qq.com>
2026-03-08 12:05:50 +0800

    Add exp23 L2-focused config and switch default train script

--- mystart_train.sh
+++ mystart_train.sh
@@ -7,4 +7,4 @@ export PYTHONPATH="/home/wudi/code/mySparseDrive/SparseDrive:${PYTHONPATH:-}"
 export CUDA_VISIBLE_DEVICES=0
 # export CUDA_LAUNCH_BLOCKING=1  # 仅调试 CUDA 报错时开启
 
-python tools/train.py projects/configs/sparsedrive_small_stage2.py "$@"
+python tools/train.py projects/configs/sparsedrive_small_stage2_exp23.py "$@"
